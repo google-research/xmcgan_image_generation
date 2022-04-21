8409c63f398988f294c216c78880f9a7b7ebe542
Jayati Naik <naik.jayati89@gmail.com>
2022-04-21 01:58:00 -0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
               ds = tfds.load('coco_captions', split=tfds_split, data_dir='/ifs/loni/faculty/thompson/four_d/jnaik/cocodataset2014/data')
               for features in tqdm(ds, position=0):
                   filename = features['image/filename']
-                  filename = bytes.decode(filename.numpy())
+                  filename = bytes.decode(filename.numpy()).split('.jpg')[0]
                   if tfds_split == 'train':
                       if filename in coco_mini_dataset:
                          print('True')

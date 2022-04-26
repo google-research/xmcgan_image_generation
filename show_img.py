1cc27bcd8b8ed92480e870f967f506252ba2aac7
jayati-naik <jnaik@usc.edu>
2022-04-25 19:49:27 -0700

    See logs

--- show_img.py
+++ show_img.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 img = np.load('generated_image.npy')
 fig = plt.figure(figsize=(8,8))
 B, H, W, C = img.shape
-print(img[0].shape)
+
 for i in range(B):
+    fig.add_subplot(1, 7, i)
     plt.imshow(img[i])
 plt.show()

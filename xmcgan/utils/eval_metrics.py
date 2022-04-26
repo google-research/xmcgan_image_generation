59df4362d61287886c3cbac62cbd6e05fc8655fd
jayati-naik <jnaik@usc.edu>
2022-04-26 11:06:49 -0700

    loggers added

--- xmcgan/utils/eval_metrics.py
+++ xmcgan/utils/eval_metrics.py
@@ -110,7 +110,7 @@ class EvalMetric:
     def jax_save(file, arr):
       def save_to_file(a, transforms):
           print("save image")
-          jax.numpy.save('/images/'+file[i], a[i])
+          jax.numpy.save('/images/'+file, a)
       hcb.id_tap(save_to_file, arr)
 
     if config.dtype == "bfloat16":
@@ -132,7 +132,6 @@ class EvalMetric:
 
     filenames = batch["filename"]
     print("Save batches")
-
     B, _, _, _ = generated_image.shape()
     for i in range(B):
       jax_save(filenames[i], generated_image[i])
